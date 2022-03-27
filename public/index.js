a502b0c3b879a4ff0c458d2d8f68500db3323f05
J <jlin9533@gmail.com>
2022-03-27 13:45:35 -0400

    tweets styles

--- public/index.js
+++ public/index.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
             } else {
                 neutral++;
             }
-            $("#tweets").append(`<p>${t.text}</p>`);
+            $("#tweets").append(`<div><p>${t.text}</p></div>`);
         }
         $('#data').append(`<h1>Tweets collected: ${tweets.length}</h1>`);
         
